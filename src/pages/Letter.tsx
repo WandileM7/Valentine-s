f96b652bc2313ddfa03b069f709650595fab982c
Wandile Paballo Mawela <wandilemawela4@gmail.com>
2025-02-09 09:50:11 +0000

    fixing music

--- src/pages/Letter.tsx
+++ src/pages/Letter.tsx
@@ -70,7 +70,7 @@ function Letter() {
         {isMuted ? <VolumeX className="w-6 h-6 text-gray-600" /> : <Volume2 className="w-6 h-6 text-gray-600" />}
       </button>
       <audio loop autoPlay muted={isMuted}>
-        <source src="/src/music/Daniel Caesar - Get You (feat. Kali Uchis).mp3" type="audio/mpeg" />
+        <source src="/music/Daniel Caesar - Get You (feat. Kali Uchis).mp3" type="audio/mpeg" />
       </audio>
 
       {/* Gallery Link */}
